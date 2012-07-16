153fc75971d11452c4e89b58ccfde358d2a68d77
Frederic Le Bris <flebris@gmail.com>
2012-07-16 11:19:01 +0200

    Ajout md5

--- src/vmod_pj.c
+++ src/vmod_pj.c
@@ -4,6 +4,9 @@
 #include "bin/varnishd/cache.h"
 
 #include "vcc_if.h"
+#include "global.h"
+#include "md5.h"
+
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
@@ -16,10 +19,23 @@ vmod_hello(struct sess *sp, const char *name)
 {
 	char *p;
 	unsigned u, v;
+	MD_CTX context;
+	unsigned char digest[16];
+	unsigned int len = strlen (name);
+
 
 	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
 	p = sp->wrk->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
+
+  	MDInit (&context);
+  	MDUpdate (&context, name, len);
+  	MDFinal (digest, &context);
+
+  	printf ("MD5 ori (\"%s\") = ", name);
+	printf ("MD5 calcul (\"%s\") = ", digest);
+  	printf ("\n");
+	
+	v = snprintf(p, u, "%s", digest);
 	v++;
 	if (v > u) {
 		/* No space, reset and leave */
